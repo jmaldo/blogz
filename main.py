fbb747245608fed6a0324c72a6419dcd0f66b7a1
Jeremy Maldonado <jeremymaldonado99@hotmail.com>
2018-01-10 14:52:04 -0600

    added column for password to database

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
     username = db.Column(db.String(150))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='user')
 
     def __init__(self, username, password):
